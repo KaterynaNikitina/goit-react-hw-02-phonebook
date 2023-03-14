48af4001749e4b598a1070b21f235f0e024322c9
Katerina <Kateryna.e.nikitina@gmail.com>
2023-03-14 23:30:30 +0200

    components addeed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+// import ListItem from './ListItem/ListItem';
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  onFormSubmit = data => {
+    console.log(data);
+    const isExistingContact = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
+
+    if (isExistingContact) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+
+    const contact = { id: nanoid(), ...data };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  onFilterChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  handleClick = data => {
+    console.log(data);
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== data),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <div
       style={{
         height: '100vh',
+        width: '400px',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 38,
         color: '#010101'
       }}
-    >
-      React homework template
-    </div>
-  );
-};
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.onFormSubmit} />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.onFilterChange} />
+        <ContactList contacts={filteredContacts} onClick={this.handleClick} />
+      </div>
+    );
+  }
+}
+
+export default App;
